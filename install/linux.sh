f1e63a4f46c4799d0331aff824b8b9b3745e6786
maurice-jo <68750310+maurice-jo@users.noreply.github.com>
2024-07-28 17:15:13 -0400

    Update linux.sh

--- install/linux.sh
+++ install/linux.sh
@@ -3,8 +3,8 @@ sudo apt update
 sudo apt install -y cmake build-essential git puredata puredata-dev python3 python3-pip
 
 # Download and install nn_tilde
-git clone https://github.com/acids-ircam/nn_tilde
-cd nn_tilde
+git clone https://github.com/maurice-jo/nn_tilde_pi.git
+cd nn_tilde_pi
 
 # Download and install libtorch
 mkdir lib
